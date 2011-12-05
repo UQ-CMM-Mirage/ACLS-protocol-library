7b4ad909fdb2faa26330e94bc04411aca61fb068
Stephen Crawley <s.crawley@uq.edu.au>
2011-12-05 17:36:43 +1000

    (Tidy imports)

--- aclslib/src/main/java/au/edu/uq/cmm/aclslib/message/AbstractReader.java
+++ aclslib/src/main/java/au/edu/uq/cmm/aclslib/message/AbstractReader.java
@@ -2,8 +2,6 @@ package au.edu.uq.cmm.aclslib.message;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
