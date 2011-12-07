c15a3b2fc6288fa0008f0ba83521abd3457c1f3a
Stephen Crawley <s.crawley@uq.edu.au>
2011-12-07 16:57:46 +1000

    New unit tests

---
+++ aclslib/src/test/java/au/edu/uq/cmm/acslib/service/ThreadServiceBaseTest.java
@@ -0,0 +1,68 @@
+package au.edu.uq.cmm.acslib.service;
+
+import java.util.Deque;
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import au.edu.uq.cmm.aclslib.service.Service;
+import au.edu.uq.cmm.aclslib.service.ThreadServiceBase;
+
+public class ThreadServiceBaseTest {
+    
+    private static class TestService extends ThreadServiceBase {
+        private final Deque<String> status;
+        private final AtomicBoolean killSwitch;
+        
+        public TestService(Deque<String> status, AtomicBoolean killSwitch) {
+            this.status = status;
+            this.killSwitch = killSwitch;
+        }
+        
+        public void run() {
+            status.add("running");
+            while (true) {
+                try {
+                    if (killSwitch.get()) {
+                        killSwitch.set(false);
+                        status.add("arrggghhh");
+                        throw new NullPointerException();
+                    }
+                    Thread.sleep(10);
+                } catch (InterruptedException ex) {
+                    break;
+                }
+            }
+            status.add("finished");
+        }
+    }
+
+    @Test
+    public void testStartupShutdown() throws InterruptedException {
+        BlockingDeque<String> status = new LinkedBlockingDeque<String>();
+        AtomicBoolean killSwitch = new AtomicBoolean();
+        Service service = new TestService(status, killSwitch);
+        Assert.assertNull(status.pollFirst());
+        service.startup();
+        Assert.assertEquals("running", status.pollFirst(2, TimeUnit.SECONDS));
+        service.shutdown();
+        Assert.assertEquals("finished", status.pollFirst(2, TimeUnit.SECONDS));
+    }
+    
+    @Test
+    public void testStartupDieShutdown() throws InterruptedException {
+        BlockingDeque<String> status = new LinkedBlockingDeque<String>();
+        AtomicBoolean killSwitch = new AtomicBoolean();
+        Service service = new TestService(status, killSwitch);
+        Assert.assertNull(status.pollFirst());
+        service.startup();
+        Assert.assertEquals("running", status.pollFirst(2, TimeUnit.SECONDS));
+        killSwitch.set(true);
+        Assert.assertEquals("arrggghhh", status.pollFirst(2, TimeUnit.SECONDS));
+        service.shutdown();
+    }
+}
