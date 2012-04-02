63ea02cf40934bcd5be4e7aa53d10e3dd862b0c2
Stephen Crawley <s.crawley@uq.edu.au>
2012-04-02 14:42:24 +1000

    Added a logout wrapper method to the ACLSProxy object

---
+++ aclslib/src/main/java/au/edu/uq/cmm/aclslib/proxy/AclsAuthenticationException.java
@@ -0,0 +1,10 @@
+package au.edu.uq.cmm.aclslib.proxy;
+
+@SuppressWarnings("serial")
+public class AclsAuthenticationException extends Exception {
+
+    public AclsAuthenticationException(String message) {
+        super(message);
+    }
+
+}
